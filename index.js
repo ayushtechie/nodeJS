954e69b192da57a5b79fdce9adf14578c7151d65
ayushtechie <techie.ayushtiwari@gmail.com>
2023-11-20 20:38:39 +0530

    Parsing Request Bodies

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-const http = require("http")
+const http = require("http");
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200, {'Content-Type': 'application/JSON'});
-    res.end(JSON.stringify({
-        data: 'hello world',
-    }));
+    // console.log(req.url, req.method, req.headers);
+    // res.writeHead(200, {'Content-Type': 'application/JSON'});
+    // res.end(JSON.stringify({
+    //     data: 'hello world',
+    // }));
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write("<html>");
+        res.write("<head><title>Enter Message</title></head>");
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Send</button></form></body>");
+        res.write("</html>");
+        return res.end();
+    }
+
+    if (url === '/message' || method === 'POST') {
+        const body = [];
+        req.on('data', (chunck) => {
+            console.log(chunck);
+            body.push(chunck);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            // const message = parsedBody.split('=')[1];
+            // fs.writeFileSync('message.txt', message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>This is from my nodeJS server</h1></body>");
+    res.write("</html>");
+    res.end();
 });
 
 server.listen(5000);
